e8b5dfde1b7665d6399c00883b7cff3d81163f42
prosantaneogi <36035942+prosantaneogi@users.noreply.github.com>
2020-08-25 12:20:29 +0530

    Add files via upload

---
+++ Activity1_3.java
@@ -0,0 +1,24 @@
+package javaActivity3;
+
+public class Activity1_3 {
+
+	public static void main(String[] args) {
+
+
+		double seconds = 1000000000;
+		double EarthSec = 31557600 ;
+		double MercurySecs = 0.2408467;
+		double VenusSec = 0.61519726;
+		double MarsSec = 1.8808158 ;
+		double JupiterSec = 11.862615;
+		double SaturnSec = 29.447498;
+		double UranusSec = 84.016846;
+		double NeptuneSec = 164.79132 ;
+		
+		System.out.println("The Age in EarthSec :" + seconds/EarthSec);
+
+	}
+	
+
+
+}
